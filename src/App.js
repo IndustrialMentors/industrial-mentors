4c8c840869ae34922f53083180a858894a22a5ed
Ravindra <ravindrasonwane1998@gmail.com>
2023-09-14 12:45:31 +0530

    Fooet Commoited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
+=======
+import React from "react";
+// import logo from "./assets/im-symbol.png";
+import "./App.css";
+>>>>>>> 4dbcfe483fc6016879bdd42ede116e963a1d3495
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/home_page/footer/footer';
 function App() {
   return (
+<<<<<<< HEAD
     <>
     <Footer/>
     </>
   );
+=======
+  <div className="App">
+    
+  </div>);
+>>>>>>> 4dbcfe483fc6016879bdd42ede116e963a1d3495
 }
 
 export default App;
