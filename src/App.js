9ba1466f8c3b688ab4eead015933ecaef41ca0d7
satyashil <satyashil>
2023-09-14 13:48:32 +0530

    hiring partners component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 // import logo from "./assets/im-symbol.png";
+import Companies from "./components/home_page/hiring slider/hiringpartners";
 import "./App.css";
 
 function App() {
   return (
   <div className="App">
-    
+    <Companies/>
   </div>);
 }
 
