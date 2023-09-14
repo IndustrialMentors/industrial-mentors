e22b1d98b7c5039e5e3c8629c0b72487d363014b
sagar201999 <smagare1111@gmail.comm>
2023-09-14 14:02:25 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import logo from "./assets/im-symbol.png";
 import "./App.css";
+import Navbar from "./components/home_page/navabar/navbar";
 
 function App() {
   return (
   <div className="App">
+    <Navbar />
     
   </div>);
 }
