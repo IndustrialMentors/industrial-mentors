4827e7c8dbdbcefffac9b45d456ea48266731f0c
vaishnavi <vaishnavi.muley44@gmail.com>
2023-09-15 12:46:10 +0530

    video intro section add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Companies from "./components/home_page/hiring slider/hiringpartners";
 import "./App.css";
 import Navbar from "./components/home_page/navabar/navbar";
 import Courses from "./components/home_page/pupular-courses/courses";
+import Intro_vid from "./components/home_page/Intro-video/Intro_vid";
 import Footer from "./components/home_page/footer/footer";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   <div className="App">
     <Navbar />
     <Courses/>
+    <Intro_vid/>
     <Companies/>
     <Footer/>
   </div>);
